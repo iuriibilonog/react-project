f1a99fe6a13c90aa9d4f404be5b80c3b6492bac5
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-11-29 17:52:58 +0200

    material added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <NavBar />
     </div>
   );
 }
