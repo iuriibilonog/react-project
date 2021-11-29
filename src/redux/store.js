118047679551c74e0a757909a00658b59710efec
Iurii Bilonog <iuriibilonog@gmail.com>
2021-11-29 16:05:07 +0200

    add structure

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+export const store = configureStore({
+  reducer: {
+    auth: null,
+    transactions: null,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
