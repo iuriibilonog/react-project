9dad46144a9ebea6d942d35c60de570dbc5ab39b
Iurii Bilonog <iuriibilonog@gmail.com>
2021-11-30 15:32:33 +0200

    add login logic

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,12 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AddUser, token } from '../../services/api';
+import { AddUser, token, LoginUser } from '../../services/api';
 
 export const register = createAsyncThunk('auth/register', async credentials => {
   try {
-    console.log(credentials);
     const { data } = await AddUser(credentials);
-    console.log(data);
-    // token.set(data.token);
+    return data;
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+export const login = createAsyncThunk('auth/login', async credentials => {
+  try {
+    const { data } = await LoginUser(credentials);
+    token.set(data.token);
     return data;
   } catch (error) {
     alert(error.message);
