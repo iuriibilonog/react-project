0b655e7e8b10af35a6eefc04f1b575461bcce27d
Artowod <myjavaname@gmail.com>
2021-12-03 20:37:38 +0200

    update balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -9,24 +9,22 @@ import { getIncomes, getExpenses } from '../../redux/transactions-selectors';
 import GoToReport from '../GoToReport';
 import { updateBalance } from '../../redux/transactions-operations';
 const Balance = () => {
-  const initbalance = useSelector(getBalance);
-  const userBalanceFromAuth = useSelector(getUserBalance);
-  const isSystemStarted = useSelector(getIsSystemInitialised);
-
-  const expenses = useSelector(getExpenses).length;
-  const incomes = useSelector(getIncomes).length;
-
-  const dispatch = useDispatch();
-  const pushIsSystemStartedMarkerToState = marker => dispatch(actions.setIsSystemStarted(marker));
-  const pushBalanceToState = newBalance => dispatch(actions.setBalance(newBalance));
-
-  const [initBalance, setInitBalance] = useState(Math.round(initbalance) + ' UAH'); //was balance in useState
-
   const [balanceState, setBalanceState] = useState('unset');
   const [timerId, setTimerId] = useState(null);
   const [isReminderShown, setIsReminderShown] = useState(false);
-
   const [isModalShown, setIsModalShown] = useState(false);
+
+  const userBalanceFromAuth = useSelector(getUserBalance);
+  const [balance, setInitBalance] = useState(Math.round(userBalanceFromAuth) + ' UAH'); //то что отображается в инпуте
+
+  const dispatch = useDispatch();
+  const pushIsSystemStartedMarkerToState = marker => dispatch(actions.setIsSystemStarted(marker));
+  const pushBalanceToState = newBalance => dispatch(actions.setBalance(newBalance)); //if it`s beginning
+
+  const initbalance = useSelector(getBalance);
+  const isSystemStarted = useSelector(getIsSystemInitialised);
+  const expenses = useSelector(getExpenses).length;
+  const incomes = useSelector(getIncomes).length;
 
   const zeroReminding = () => {
     const timerId = setTimeout(() => {
@@ -48,7 +46,7 @@ const Balance = () => {
   };
 
   useEffect(() => {
-    if (isSystemStarted || expenses || incomes) {
+    if (userBalanceFromAuth || isSystemStarted || expenses || incomes) {
       console.log('userBalanceFromAuth', userBalanceFromAuth);
       pushBalanceToState(userBalanceFromAuth); //  - to state only
       setInitBalance(userBalanceFromAuth);
@@ -81,7 +79,7 @@ const Balance = () => {
   };
 
   const prepareDataForBackend = () => {
-    const balanceDigit = initBalance.slice(0, initBalance.length - 4).trim();
+    const balanceDigit = balance.slice(0, balance.length - 4).trim();
     if (Number(balanceDigit)) {
       console.log('Output Balance :', balanceDigit);
       finalInitializing();
@@ -134,7 +132,7 @@ const Balance = () => {
     return isEnabled ? (
       <input
         type="input"
-        value={initBalance}
+        value={balance}
         className={s.balance}
         onChange={changeBalanceHandler}
         onClick={clickBalanceHandler}
@@ -143,7 +141,7 @@ const Balance = () => {
     ) : (
       <input
         type="input"
-        value={initBalance}
+        value={balance}
         className={s.balance}
         onChange={changeBalanceHandler}
         onClick={clickBalanceHandler}
@@ -152,6 +150,150 @@ const Balance = () => {
       />
     );
   };
+
+  // const initbalance = useSelector(getBalance);
+  // const userBalanceFromAuth = useSelector(getUserBalance);
+  // const isSystemStarted = useSelector(getIsSystemInitialised);
+
+  // const expenses = useSelector(getExpenses).length;
+  // const incomes = useSelector(getIncomes).length;
+
+  // const dispatch = useDispatch();
+  // const pushIsSystemStartedMarkerToState = marker => dispatch(actions.setIsSystemStarted(marker));
+  // const pushBalanceToState = newBalance => dispatch(actions.setBalance(newBalance));
+
+  // const [initBalance, setInitBalance] = useState(Math.round(initbalance) + ' UAH'); //was balance in useState
+
+  // const [balanceState, setBalanceState] = useState('unset');
+  // const [timerId, setTimerId] = useState(null);
+  // const [isReminderShown, setIsReminderShown] = useState(false);
+
+  // const [isModalShown, setIsModalShown] = useState(false);
+
+  // const zeroReminding = () => {
+  //   const timerId = setTimeout(() => {
+  //     setIsReminderShown(true);
+  //   }, 4000);
+  //   setTimerId(timerId);
+  // };
+
+  // const confirmBtnMarkup = isEnabled => {
+  //   return isEnabled ? (
+  //     <button type="submit" className={s.confirmBtn}>
+  //       Подтвердить
+  //     </button>
+  //   ) : (
+  //     <button type="submit" className={s.confirmBtn_disabled} disabled>
+  //       Подтвердить
+  //     </button>
+  //   );
+  // };
+
+  // useEffect(() => {
+  //   if (isSystemStarted || expenses || incomes) {
+  //     console.log('userBalanceFromAuth', userBalanceFromAuth);
+  //     pushBalanceToState(userBalanceFromAuth); //  - to state only
+  //     setInitBalance(userBalanceFromAuth);
+  //     setBalanceState('set');
+  //   } else if (balanceState === 'unset') {
+  //     zeroReminding();
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  // useEffect(() => {
+  //   if (balanceState === 'set') {
+  //     clearTimeout(timerId);
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [balanceState]);
+
+  // useEffect(() => {
+  //   setInitBalance(userBalanceFromAuth);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [userBalanceFromAuth]);
+
+  // const responseHandling = response => {
+  //   setIsModalShown(false);
+  //   console.log(response);
+  //   if (response) {
+  //     prepareDataForBackend();
+  //   }
+  //   return response;
+  // };
+
+  // const prepareDataForBackend = () => {
+  //   const balanceDigit = initBalance.slice(0, initBalance.length - 4).trim();
+  //   if (Number(balanceDigit)) {
+  //     console.log('Output Balance :', balanceDigit);
+  //     finalInitializing();
+  //     sendDataToState(Number(balanceDigit));
+  //   } else {
+  //     console.log('Wrong balance!');
+  //     setInitBalance('0 UAH');
+  //   }
+  // };
+
+  // const finalInitializing = () => {
+  //   setIsReminderShown('false');
+  //   setBalanceState('set');
+  // };
+
+  // const sendDataToState = balanceDigit => {
+  //   /* Send marker and balance to STATE */
+  //   pushIsSystemStartedMarkerToState(true);
+  //   //pushBalanceToState(userBalanceFromAuth); //  - to state only
+  //   dispatch(
+  //     updateBalance({
+  //       newBalance: balanceDigit,
+  //     }),
+  //   );
+  // };
+
+  // const submitBalanceHandler = event => {
+  //   event.preventDefault();
+  //   setIsModalShown(true);
+  // };
+
+  // const clickBalanceHandler = event => {
+  //   if (event.target.value === '0 UAH') {
+  //     setInitBalance('');
+  //   } else if (event.target.value.slice(event.target.value.length - 3) === 'UAH')
+  //     setInitBalance(event.target.value.slice(0, event.target.value.length - 4));
+  // };
+
+  // const looseFocusBalanceHandler = event => {
+  //   if (event.target.value === '') {
+  //     setInitBalance('0 UAH');
+  //   } else setInitBalance(prev => prev + ' UAH');
+  // };
+
+  // const changeBalanceHandler = event => {
+  //   setInitBalance(event.target.value.trim());
+  // };
+
+  // const inputMarkup = isEnabled => {
+  //   return isEnabled ? (
+  //     <input
+  //       type="input"
+  //       value={initBalance}
+  //       className={s.balance}
+  //       onChange={changeBalanceHandler}
+  //       onClick={clickBalanceHandler}
+  //       onBlur={looseFocusBalanceHandler}
+  //     />
+  //   ) : (
+  //     <input
+  //       type="input"
+  //       value={initBalance}
+  //       className={s.balance}
+  //       onChange={changeBalanceHandler}
+  //       onClick={clickBalanceHandler}
+  //       onBlur={looseFocusBalanceHandler}
+  //       disabled
+  //     />
+  //   );
+  // };
 
   return (
     <div className={s.wrapper}>
