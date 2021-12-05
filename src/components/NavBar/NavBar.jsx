0e9c8ecd2954d62bd8ce7d3445433da55f7dcf48
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-12-05 18:55:42 +0200

    calendar icon fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,24 +12,20 @@ import Divider from '@mui/material/Divider';
 import { StyledBadge } from './StyledBadge';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logOut } from '../../redux/auth/auth-operations';
+import iconUser from '../../img/iconUser.svg';
 
-
-
-
-
-
-const NavBar = ({socialName, socialImg, isAuthFromSocial }) => {
+const NavBar = ({ socialName, socialImg, isAuthFromSocial }) => {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   const socialAuth = useSelector(state => state.auth.socialAuth);
-  const loginNameForm = useSelector(state => state.auth.user?.userData?.email)
-  const loginNamefromGoogle = useSelector(state => state.auth.user?.email)
+  const loginNameForm = useSelector(state => state.auth.user?.userData?.email);
+  const loginNamefromGoogle = useSelector(state => state.auth.user?.email);
   const loginName = loginNameForm || loginNamefromGoogle;
   // console.log(loginName.length)
   const dispatch = useDispatch();
 
   // const loginFromEmail = () => {
   //   for (let i = 0; i <= loginName.length; i++) {
-      
+
   //   }
   // }
   return (
@@ -53,7 +49,12 @@ const NavBar = ({socialName, socialImg, isAuthFromSocial }) => {
                     variant="dot"
                     sx={{ mr: 2 }}
                   >
-                    <Avatar alt="Remy Sharp" src={isAuthFromSocial && socialAuth ? socialImg: "/static/images/avatar/1.jpg"} />
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={
+                        isAuthFromSocial && socialAuth ? socialImg : '/static/images/avatar/1.jpg'
+                      }
+                    />
                   </StyledBadge>
                 </Stack>
                 <Typography
@@ -81,28 +82,26 @@ const NavBar = ({socialName, socialImg, isAuthFromSocial }) => {
                   className={s.visibility}
                 />
                 <div onClick={() => dispatch(logOut())}>
-                <Typography
-                  variant="text"
-                  sx={{
-                    color: 'white',
-                    textDecoration: 'underline',
-                    textTransform: 'capitalize',
-                    color: '#52555F',
-                    fontFamily: 'Roboto',
-                    padding: '10px',
-                    fontSize: '12px',
-                    cursor: 'pointer',
-                    '&:hover,&:focus': {
-                      color: "#ff751d"
-                    }
-
-                  }}
-                  className={s.visibility}
-                  
-                >
-                  Выйти
-                </Typography>
-                <img className={s.logoutIcon} src={logout} />
+                  <Typography
+                    variant="text"
+                    sx={{
+                      color: 'white',
+                      textDecoration: 'underline',
+                      textTransform: 'capitalize',
+                      color: '#52555F',
+                      fontFamily: 'Roboto',
+                      padding: '10px',
+                      fontSize: '12px',
+                      cursor: 'pointer',
+                      '&:hover,&:focus': {
+                        color: '#ff751d',
+                      },
+                    }}
+                    className={s.visibility}
+                  >
+                    Выйти
+                  </Typography>
+                  <img className={s.logoutIcon} src={logout} />
                 </div>
               </div>
             )}
